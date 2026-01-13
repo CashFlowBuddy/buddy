94d92a06ffb27fb86016b085b7c887acdd18c5d5
Barsika221 <kovacs.bars@gmail.com>
2026-01-13 13:14:55 +0100

    feat: implement authentication client and social login button

---
+++ src/components/ui/socialLoginBtn.tsx
@@ -0,0 +1,21 @@
+import { Button } from "react-native-paper";
+import { useState } from "react";
+import { authClient } from "../../../lib/auth-client";
+
+
+export default function SocialLoginBtn() {
+    const handleLogin = async () => {
+        try {
+            await authClient.signIn.social({
+                provider: 'github',
+            });
+        } catch (error) {
+        }
+    }
+
+    return (
+        <Button onPress={handleLogin}>
+            Continue with GitHub
+        </Button>
+    );
+}
